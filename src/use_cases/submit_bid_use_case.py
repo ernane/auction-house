09168977cac396a668b30d369998551b5c14ca4d
Ernane Sena <ernane.sena@gmail.com>
2024-09-17 00:17:32 -0300

    feat: Add SubmitBidUseCase to handle bid submission
    This commit adds the SubmitBidUseCase class, which handles the submission of bids. It takes in an auction repository and checks if the auction exists before allowing the bid to be submitted. If the auction does not exist, it raises an AuctionNotFoundError exception.

---
+++ src/use_cases/submit_bid_use_case.py
@@ -0,0 +1,13 @@
+from src.domain.entities.bid import Bid
+from src.ports.repositories.auction_repository import AuctionRepository
+from src.use_cases.exceptions import AuctionNotFoundError
+
+
+class SubmitBidUseCase:
+    def __init__(self, auction_repository: AuctionRepository):
+        self._auction_repository = auction_repository
+
+    async def __call__(self, bid: Bid) -> None:
+        auction = await self._auction_repository.get(id=bid.auction_id)
+        if not auction:
+            raise AuctionNotFoundError(bid.auction_id)
